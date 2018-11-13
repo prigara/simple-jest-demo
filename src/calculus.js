240bf81f8bcf855e868f4b998bfe7b723139418f
Ekaterina Prigara <ekaterina@prigara.com>
2018-11-13 20:57:55 +0100

    Added JSDoc

--- src/calculus.js
+++ src/calculus.js
@@ -1,3 +1,9 @@
+/**
+ * Adds two integers
+ * @param a {number} - First number
+ * @param b {number} - Second number
+ * @returns {number} - Sum of two number
+ */
 export function add(a, b) {
     if (Number.isInteger(a) && Number.isInteger(b)) {
         return a + b;
@@ -5,6 +11,12 @@ export function add(a, b) {
     else throw "Floats are not supported"
 }
 
+/**
+ * Divides one number by another
+ * @param a {number} - First number
+ * @param b {number} - Second number
+ * @returns {*} - Result of division, rounded up to 2 decimal places
+ */
 export function divide(a, b) {
     if (b === 0) {
         throw Error("Can't divide by zero")
